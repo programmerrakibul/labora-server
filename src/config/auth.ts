e937dc6b06a9868aac89634abd08635f04dee214
Programmer Rakibul <rakibul00206@gmail.com>
2026-08-06 18:23:33 +0600

    feat: update package.json and vercel.json for improved build configuration and add advanced cookie settings in auth configuration

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -30,6 +30,15 @@ export const initAuth = () => {
     },
 
     trustedOrigins: [env.CLIENT_URL],
+    advanced: {
+      useSecureCookies: true,
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+        partitioned: true,
+      },
+    },
 
     databaseHooks: {
       user: {
